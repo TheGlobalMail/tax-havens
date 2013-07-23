0961d54804458fb1d88aa94948221b70d6547aad
Mark Finger <markfinger@gmail.com>
2013-07-23 18:19:28 +1000

    mo data

--- app/js/data.js
+++ app/js/data.js
@@ -14,5 +14,300 @@ tgm.data = {
     "Jersey": 1,
     "Mauritius": 1,
     "Singapore": 7
+  },
+  "Ansell Limited": {
+    "Singapore": 4,
+    "Netherlands Antilles": 2,
+    "Hong Kong": 2,
+    "Mauritius": 1
+  },
+  "Australia and New Zealand Banking Group Limited": {
+    "Singapore": 2,
+    "Hong Kong": 2
+  },
+  "Alumina Limited": {
+    "Hong Kong": 1
+  },
+  "AXA": {
+    "Luxembourg": 1,
+    "Hong Kong": 1,
+    "Singapore": 1,
+    "Switzerland": 1,
+    "Philippines": 1
+  },
+  "BHP Billiton Limited": {
+    "British Virgin Islands": 1,
+    "Anguilla": 1,
+    "Switzerland": 2
+  },
+  "Boral Limited": {
+    "Singapore": 2,
+    "Jersey": 1
+  },
+  "Boart Longyear Limited": {
+    "Hong Kong": 1
+  },
+  "Bluescope Steel Limited": {
+    "Brunei": 1,
+    "Hong Kong": 1,
+    "Mauritius": 1,
+    "Panama": 1,
+    "Philippines": 1,
+    "Singapore": 5,
+    "Vanuatu": 1
+  },
+  "Commonwealth Bank of Australia": {
+    "Hong Kong": 3,
+    "Cayman Islands": 1,
+    "Singapore": 2,
+    "Luxembourg": 1,
+    "Bermuda": 1
+  },
+  "Coca Cola Amatil Limited": {
+    "Singapore": 1
+  },
+  "Challenger Limited": {
+    "Jersey": 4,
+    "Luxembourg": 3,
+    "Bermuda": 1,
+    "Singapore": 1
+  },
+  "Cochlear Limited": {
+    "Switzerland": 1,
+    "Hong Kong": 1,
+    "Singapore": 1
+  },
+  "Computershare Limited": {
+    "Austria": 1,
+    "Hong Kong": 4,
+    "Singapore": 1,
+    "UAE": 1,
+    "British Virgin Islands": 1,
+    "Cayman Islands": 1,
+    "Jersey": 6,
+    "Guernsey": 1,
+    "Isle of Man": 1,
+    "Bermuda": 1
+  },
+  "CSL Limited": {
+    "Switzerland": 1,
+    "Hong Kong": 2
+  },
+  "CSR Limited": {
+    "Hong Kong": 1,
+    "Singapore": 2
+  },
+  "Crown Limited": {
+    "Luxembourg": 1,
+    "Cayman Islands": 1,
+    "Bahamas": 1
+  },
+  "Downer EDI Limited": {
+    "Singapore": 22,
+    "Philippines": 1,
+    "Hong Kong": 7,
+    "Vanuatu": 1,
+    "British Virgin Islands": 1
+  },
+  "Foster's Group Limited": {
+    "Singapore": 1,
+    "Turks and Caicos Islands": 1
+  },
+  "Fortescue Metals Group Limited": {
+    "Singapore": 1
+  },
+  "Fairfax Media Limited": {
+    "Singapore": 2
+  },
+  "Goodman Fielder Limited": {
+    "Hong Kong": 3,
+    "Philippines": 1
+  },
+  "Goodman Group": {
+    "Cayman Islands": 7,
+    "Luxembourg": 29,
+    "Hong Kong": 2,
+    "Jersey": 23,
+    "Singapore": 5,
+    "UAE": 1
+  },
+  "Harvey Norman Holdings Limited": {
+    "Singapore": 4
+  },
+  "Insurance Australia Group Limited": {
+    "Singapore": 4,
+    "Gibraltar": 1
+  },
+  "Investa Office Fund": {
+    "Luxembourg": 2
+  },
+  "Incitec Pivot Limited": {
+    "Singapore": 2,
+    "Hong Kong": 3
+  },
+  "Leighton Holdings Limited": {
+    "Hong Kong": 1
+  },
+  "Macquarie Group Limited": {
+    "Bermuda": 2
+  },
+  "National Australia Bank": {
+    "Hong Kong": 1
+  },
+  "Newcrest Mining Limited": {
+    "Singapore": 2
+  },
+  "News Corporation": {
+    "Belize": 1,
+    "Bermuda": 1,
+    "British Virgin Islands": 62,
+    "Cayman Islands": 33,
+    "Hong Kong": 21,
+    "Luxembourg": 4,
+    "Marshall Islands": 1,
+    "Mauritius": 15,
+    "Panama": 1,
+    "Singapore": 5,
+    "Switzerland": 2
+  },
+  "Origin Energy Limited": {
+    "Cook Islands ": 1,
+    "Vanuatu": 2,
+    "Samoa": 1,
+    "Panama": 1,
+    "Singapore": 9
+  },
+  "Orica Limited": {
+    "Hong Kong": 6,
+    "Singapore": 6,
+    "Jersey": 1,
+    "Panama": 4,
+    "UAE": 1,
+    "Switzerland": 1,
+    "Austria": 1,
+    "Dominican Republic": 1,
+    "Philippines": 4
+  },
+  "Oil Search Limited": {
+    "British Virgin Islands": 5,
+    "Cayman Islands": 1
+  },
+  "Onesteel Limited": {
+    "Hong Kong": 4,
+    "Philippines": 2,
+    "Singapore": 3
+  },
+  "OZ Minerals Limited": {
+    "Singapore": 1
+  },
+  "Paladin Energy Limited": {
+    "Switzerland": 1
+  },
+  "Primary Healthcare Limited": {
+    "Singapore": 1
+  },
+  "Qantas Airways Limited": {
+    "Cayman Islands": 2,
+    "Singapore": 15,
+    "Hong Kong": 3,
+    "Philippines": 1
+  },
+  "QBE Insurance Group Limited": {
+    "Switzerland": 2,
+    "Bermuda": 3,
+    "Cayman Islands": 3,
+    "Singapore": 3,
+    "Jersey": 5,
+    "Hong Kong": 3,
+    "Philippines": 1,
+    "Vanuatu": 1,
+    "Panama": 1
+  },
+  "Ramsay Healthcare Limited": {
+    "British Virgin Islands": 1,
+    "Guernsey": 1
+  },
+  "Riversdale Mining Limited": {
+    "Mauritius": 6
+  },
+  "Seek Limited": {
+    "Cayman Islands": 1
+  },
+  "Sims Metal Management Limited": {
+    "Mauritius": 1,
+    "Hong Kong": 1,
+    "Singapore": 1
+  },
+  "Santos Limited": {
+    "Singapore": 1
+  },
+  "Transurban Group": {
+    "Bermuda": 1
+  },
+  "Telecom Corporation of New Zealand Limited": {
+    "Bermuda": 1,
+    "Cook Islands": 1
+  },
+  "Ten Network Holdings Limited": {
+    "Singapore": 2,
+    "Hong Kong": 1
+  },
+  "Telstra Corporation Limited": {
+    "Bermuda": 4,
+    "Cayman Islands": 3,
+    "British Virgin Islands": 6,
+    "Hong Kong": 4,
+    "Mauritius": 1,
+    "Jersey": 1
+  },
+  "Toll Holdings Limited": {
+    "Hong Kong": 24,
+    "Macau": 1,
+    "Singapore": 26,
+    "Luxembourg": 2,
+    "British Virgin Islands": 5,
+    "UAE": 1,
+    "Bermuda": 2,
+    "Philippines": 1,
+    "Isle of Man": 1,
+    "Switzerland": 1
+  },
+  "Transfield Services Limited": {
+    "Mauritius": 1,
+    "Singapore": 1,
+    "UAE": 3
+  },
+  "UGL Limited": {
+    "Singapore": 13
+  },
+  "Westpac Banking Corporation": {
+    "Jersey": 1,
+    "Luxembourg": 1,
+    "Hong Kong": 2,
+    "Singapore": 1
+  },
+  "Westfield Group": {
+    "Jersey": 54,
+    "Luxembourg": 1,
+    "Singapore": 1
+  },
+  "Wesfarmers Limited": {
+    "Hong Kong": 1,
+    "Singapore": 1
+  },
+  "Worleyparsons Limited": {
+    "Singapore": 8,
+    "Brunei": 1,
+    "Hong Kong": 3,
+    "UAE": 1,
+    "Cayman islands": 2,
+    "Philippines": 1,
+    "Bermuda": 1
+  },
+  "Woolworths Limited": {
+    "Hong Kong": 2
+  },
+  "Woodside Petroleum Limited": {
+    "Singapore": 3
   }
 };
